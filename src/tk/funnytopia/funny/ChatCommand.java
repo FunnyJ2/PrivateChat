b225bd3a332d24dff4d6cce2d7b20f6611472e5b
Iam Notfunny <akselondrugs@gmail.com>
2011-11-12 12:24:17 +0100

    hey

---
+++ src/tk/funnytopia/funny/ChatCommand.java
@@ -0,0 +1,53 @@
+package tk.funnytopia.funny;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ChatCommand extends PrivateChat {
+	
+private PrivateChat plugin;
+	
+	public ChatCommand(PrivateChat instance) {
+		this.plugin = instance;
+	}
+	
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+		
+		if(sender instanceof Player) {
+			//player
+			final Player player = (Player) sender;
+			if(args.length < 1) {
+				//too short args
+				sender.sendMessage(ChatColor.RED + "Too few arguments! usage: /chat <message>");
+			} else {
+				//long enough args
+				if(sender.isOp() || sender.hasPermission("privatechat.chat")) {
+					//player is op
+					String playerSender = player.getName();
+					final String message = plugin.combineSplit(0, args, " ");
+					plugin.adminChatMsg(message, playerSender);
+				} else {
+					//player isn't op
+					sender.sendMessage(ChatColor.RED+"You don't have permission to do this!");
+				}
+			}
+			
+		}  else {
+			//console
+			if(args.length < 1) {
+				//too few args
+				plugin.logInfo("Too few arguments! /chat <message>");
+			} else {
+				//console - enough args
+				final String message = plugin.combineSplit(0, args, " ");
+				plugin.adminChatMsg(message, "*CONSOLE*");
+			}
+		}
+		
+		
+		return true;
+	}
+
+}
---
+++ src/tk/funnytopia/funny/PrivateChat.java
@@ -0,0 +1,56 @@
+package tk.funnytopia.funny;
+
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PrivateChat extends JavaPlugin {
+
+	
+	
+	public String combineSplit(int startIndex, String[] string, String seperator) {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = startIndex; i < string.length; i++) {
+            builder.append(string[i]);
+            builder.append(seperator);
+        }
+        builder.deleteCharAt(builder.length() - seperator.length());
+        return builder.toString();
+    }
+	
+	public void logInfo(String message) {
+    	log.info("[PrivateChat] " + message);
+    }
+	
+	public void adminChatMsg(String message, String playername) {
+        for (final Player player : this.getServer().getOnlinePlayers()) {
+            if (player.isOp() || player.hasPermission("privatechat.chat")) {
+                player.sendMessage(ChatColor.GOLD + "[Private]" + ChatColor.WHITE + "<" + ChatColor.LIGHT_PURPLE + playername + ChatColor.WHITE + "> " + ChatColor.WHITE + message);
+                this.logInfo(playername + ": " + message);
+            }
+        }
+    }
+	
+	Logger log = Logger.getLogger("Minecraft");
+	
+	@Override
+	public void onDisable() {
+		this.logInfo("Disabled");
+	}
+
+	@Override
+	public void onEnable() {
+		
+		PluginDescriptionFile pdfFile = this.getDescription();
+		
+		this.getCommand("chat").setExecutor(new ChatCommand(this));
+		
+		this.logInfo(pdfFile.getVersion() + " enabled");
+	}
+	
+	
+
+}
